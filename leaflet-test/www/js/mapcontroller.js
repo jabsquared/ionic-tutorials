eb72179018a32e49526c7e1e3860d0c0017c4d40
Bryan Rivera <bryanrivera@studentrnd.org>
2015-07-16 17:49:56 -0700

    Bounds example
    We tested the bounds on the map

--- leaflet-test/www/js/mapcontroller.js
+++ leaflet-test/www/js/mapcontroller.js
@@ -1,11 +1,23 @@
 angular.module('starter.mapcontroller', ['leaflet-directive'])
 
-.controller("MapController", [ "$scope", function($scope){
+.controller("MapController", ["$scope", "leafletData", "leafletBoundsHelpers", function($scope, leafletData, leafletBoundsHelpers) {
+  var bounds = leafletBoundsHelpers.createBoundsFromArray([
+    [47.299445428173854, -122.21683323383331],
+    [47.299445428173854, -122.21683323383331]
+  ]);
+
   angular.extend($scope, {
-    auburn: {
-      lat: 47.298863,
-      lng: -122.216050,
-      zoom: 18
-    }
+    bounds: bounds,
+
+      auburn: {
+        // lat: 47.298863,
+        // lng: -122.216050,
+        // zoom: 16,
+        // minZoom: 10,
+        // maxZoom: 21
+      }
+      // controls: {
+      //   scale: false
+      // }
   });
 }]);
