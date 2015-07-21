aea3109555d9438705076c7b5faf5130f3920968
bpshonyak <bpshonyak@live.com>
2015-07-20 23:23:59 -0700

    added popup for reminder scheduling

--- events/www/js/controllers.js
+++ events/www/js/controllers.js
@@ -5,16 +5,53 @@ app.controller('EventsCtrl', ['$scope', '$cordovaLocalNotification', '$ionicPlat
   $scope.shouldShowDelete = false;
   $scope.listCanSwipe = true;
 
+  $scope.foo = 0;
+
   //Local Notification Shit
 
-  $scope.add = function() {
+  $scope.test = function() {
+    // prompt user for reminder options
+    var alarmPopup = $ionicPopup.show({
+      title: "Set Reminder",
+      // template: "",
+      buttons: [{ // Array[Object] (optional). Buttons to place in the popup footer.
+        text: '10 min',
+        type: 'button-default',
+        onTap: function(e) {
+          // Returning a value will cause the promise to resolve with the given value.
+          add(5);
+          return 10;
+        }
+      }, {
+        text: '15 min',
+        type: 'button-positive',
+        onTap: function(e) {
+          // Returning a value will cause the promise to resolve with the given value.
+          add(10);
+          return 15;
+        }
+      }, {
+        text: '30 min',
+        type: 'button-positive',
+        onTap: function(e) {
+          // Returning a value will cause the promise to resolve with the given value.
+          return 30;
+        }
+      }]
+    });
+    console.log("alarmPopup:");
+    console.log(alarmPopup);
+  }
+
+  console.log("foo:");
+  console.log($scope.foo);
+
+  function add(sec) {
     console.log("entered add function");
+
     // create var for current time
     var now = new Date().getTime();
-    var _5SecondsFromNow = new Date(now + 5 * 1000);
-    // trigger notification 1 minute after activated
-    // alarmTime.setMinutes(alarmTime.getMinutes() + 0.5);
-    // create notification
+    var _5SecondsFromNow = new Date(now + sec * 1000);
 
     $cordovaLocalNotification.schedule({
       id: 1,
