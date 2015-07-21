5f49b9e38b81e67fa18acfa25296637f77a73d68
bpshonyak <bpshonyak@live.com>
2015-07-20 19:37:07 -0700

    back to the start -_-

--- events/www/js/controllers.js
+++ events/www/js/controllers.js
@@ -5,41 +5,48 @@ app.controller('EventsCtrl', ['$scope', '$cordovaLocalNotification', '$ionicPlat
   $scope.shouldShowDelete = false;
   $scope.listCanSwipe = true;
 
-  $ionicPlatform.ready(function() {
-    //Local Notification Shit
+  //Local Notification Shit
 
-    $scope.add = function() {
-      console.log("entered add function");
-      // create var for current time
-      var now = new Date().getTime();
-      var _5SecondsFromNow = new Date(now + 5 * 1000);
-      // trigger notification 1 minute after activated
-      alarmTime.setMinutes(alarmTime.getMinutes() + 0.5);
-      // create notification
-      $cordovaLocalNotification.schedule({
-        id: 1234,
-        // set execution time
-        message: "Bogdan is a boss",
-        title: "bow down peseant",
-        text: "TESTEST",
-        at: _5SecondsFromNow,
-        autoCancel: true,
-        sound: null
-      }).then(function() {
-        var alertPopup = $ionicPopup.alert({
-          title: "Alert has been Set",
-          template: "Thanks!"
-        });
-        alertPopup.then(function(res) {
-        });
+  $scope.add = function() {
+    console.log("entered add function");
+    // create var for current time
+    var now = new Date().getTime();
+    var _5SecondsFromNow = new Date(now + 5 * 1000);
+    // trigger notification 1 minute after activated
+    // alarmTime.setMinutes(alarmTime.getMinutes() + 0.5);
+    // create notification
+
+    // window.plugin.notification.local.add({
+    //   id: "1234", // A unique id of the notifiction
+    //   title: "Victory", // The title of the message
+    //   message: "It Works!", // The message that is displayed
+    //   date: _5SecondsFromNow // This expects a date object
+    // });
+
+    $cordovaLocalNotification.schedule({
+      id: 22,
+      title: 'test',
+      text: 'works!',
+      at: _5SecondsFromNow
+    }).then(function() {
+      var alertPopup = $ionicPopup.alert({
+        title: "Alert has been Set",
+        template: "Thanks!"
       });
-
-    }
-
-    $scope.$on("$cordovaLocalNotification:added", function(id, state, json) {
-      alert("Added a notification");
+      alertPopup.then(function(res) {});
     });
 
+  }
+
+  // window.plugin.notification.local.onadd = function(id, state, json) {
+  //   var alertPopup = $ionicPopup.alert({
+  //       title: "Alert has been Set",
+  //       template: "Thanks!"
+  //     });
+  // };
+
+  $scope.$on("$cordovaLocalNotification:added", function(id, state, json) {
+    alert("Added a notification");
   });
 
 }]);
