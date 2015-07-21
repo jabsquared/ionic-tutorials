d90aef2e777e59e24a8e257bbb872e9319af246d
bpshonyak <bpshonyak@live.com>
2015-07-20 17:58:59 -0700

    added dynamic height to events

--- events/www/js/services.js
+++ events/www/js/services.js
@@ -5,10 +5,23 @@ app.factory('event_data', function() {
         name      : "Event 1",
         type      : "music",
         start     : 7,
-        dur       : 60, // in minutes
+        dur       : 30, // in minutes
         desc      : "Musical event",
-
       },
+      {
+        name      : "Event 2",
+        type      : "speech",
+        start     : 2,
+        dur       : 60, // in minutes
+        desc      : "Speaker",
+      },
+      {
+        name      : "Event 3",
+        type      : "music",
+        start     : 6,
+        dur       : 20, // in minutes
+        desc      : "Louis and the Amigos",
+      }
     ]
     return events;
  });
