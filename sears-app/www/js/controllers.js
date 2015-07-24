8f5abcdb7e671268b80a47d76ecceddfe3867b98
bpshonyak <bpshonyak@live.com>
2015-07-24 15:30:06 -0700

    added come shit to tickets page and services

--- sears-app/www/js/controllers.js
+++ sears-app/www/js/controllers.js
@@ -2,4 +2,19 @@
 
 app.controller('TicketsCtrl', ['$scope', 'ticket_data', function($scope, ticket_data) {
   $scope.tickets = ticket_data.all();
+
+
+
+  $scope.color_stat = function (ticket){
+    if (ticket.status === 'Pending') {
+      return '#AFC4C5';
+    } else if (ticket.status === 'Complete') {
+      return '#00FF99';
+    } else if (ticket.status === 'In Progress'){
+      return '#FFFF66';
+    } else {
+      return 'red';
+    }
+  }
+
 }]);
--- sears-app/www/js/services.js
+++ sears-app/www/js/services.js
@@ -1,25 +1,59 @@
-app.factory('ticket_data', function() {
+app.factory('product_data', ['$http', function($http){
+
+  var getinfo = function(prodnum) {
+    // Simple GET request example :
+    $http.get('https://bpshonyak-prod.apigee.net/hello-world/sears?type=details&prodnum=' + prodnum).
+      success(function(data, status, headers, config) {
+        console.log(data);
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+  }
+
+  return "http://c.shld.net/rpx/i/s/i/spin/image/spin_prod_170845201";
+
+}])
+
+app.factory('ticket_data', ['product_data', function(product_data) {
   console.log('in service!');
     var tickets = [
       {
         id        : 1,
-        text      : 'ticket 1'
+        tech      : 'Bogdan',
+        status    : 'Pending',
+        prodnum   : '04649599000P',
+        img       : 'http://c.shld.net/rpx/i/s/i/spin/image/spin_prod_170845201'
       },
       {
         id        : 2,
-        text      : 'ticket 2'
+        tech      : 'Louis',
+        status    : 'Pending',
+        prodnum   : '02211000000P',
+        img       : 'http://c.shld.net/rpx/i/s/i/spin/image/spin_prod_170845201'
       },
       {
         id        : 3,
-        text      : 'ticket 3'
+        tech      : 'Jim',
+        status    : 'Complete',
+        prodnum   : '02615000000P',
+        img       : 'http://c.shld.net/rpx/i/s/i/spin/image/spin_prod_170845201'
+      },
+      {
+        id        : 4,
+        tech      : 'Brain',
+        status    : 'In Progress',
+        prodnum   : '04622442000P',
+        img       : 'http://c.shld.net/rpx/i/s/i/spin/image/spin_prod_170845201'
       }
     ]
     return {
       all: function() {
         return tickets;
       },
-      remove: function(events) {
-        tickets.splice(events.indexOf(ticket), 1);
+      add: function(events, obj) {
+        tickets.pop(obj);
       },
       get: function(id) {
         for (var i = 0; i < tickets.length; i++) {
@@ -30,4 +64,4 @@ app.factory('ticket_data', function() {
         return null;
       }
     };
- });
+ }]);
