fdb0ed7455fc52f717944d9b2d3a810380c74523
Louis Vichy <lgvichy@gmail.com>
2015-07-25 17:43:28 -0700

    Hopefully it works by 6

---
+++ vidcap/www/js/controllers.js
@@ -0,0 +1,29 @@
+angular.module('starter.controllers', ['ngCordova'])
+  .controller('VideoCtrl', function($scope, $cordovaCapture, VideoService) {
+
+    $scope.clip = '';
+
+    $scope.captureVideo = function() {
+      $cordovaCapture.captureVideo().then(function(vidData) {
+        VideoService.saveVideo(vidData).success(function(data) {
+          $scope.clip = data;
+          $scope.$apply();
+        }).error(function(data) {
+          // body...
+          console.log('ERROR; ' + data);
+        })
+      })
+    }
+
+    $scope.urlForClipThumb = function(clipUrl) {
+      var name = clipUrl.substr(clipUrl.lastIndexOf('/') + 1);
+      var trueOrigin = cordova.file.dataDirectory + name;
+      var sliced = trueOrigin.slice(0, -4);
+      return sliced + '.png';
+    }
+
+    $scope.showClip = function(clip) {
+      console.log('show clip: ' + clip);
+    }
+
+  });
---
+++ vidcap/www/js/services.js
@@ -0,0 +1,70 @@
+angular.module('starter.services', [])
+  .service('VideoService', function($q) {
+    var deferred = $q.defer();
+    var promise = deferred.promise;
+
+    promise.success = function(fun) {
+      promise.then(fun);
+      return promise;
+    }
+
+    promise.error = function(fun) {
+      promise.then(null, fun);
+      return promise;
+    }
+
+    function createFileEntry(fileURI) {
+      window.resolveLocalFileSystemURL(fileURI, function(entry) {
+        return copyFile(entry);
+        // body...
+      }, fail);
+    }
+
+    function copyFile(fileEntry) {
+      var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
+
+      var newName = makeid() + name;
+
+      window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(storagePath) {
+        fileEntry.copyTo(storagePath, newName, function(succ) {
+          return onCopySuccess(succ);
+        }, fail);
+      })
+
+    }
+
+    function onCopySuccess(entry) {
+      var name = entry.nativeURL.slice(0, -4);
+      window.PKVideoThumbnail.createThumbnail(entry.nativeURL, name + '.png', function(prevSucc) {
+        return prevImageSuccess(prevSucc);
+      }, fail);
+    }
+
+    function prevImageSuccess(succ) {
+      var name = entry.nativeUrl = succ.slide(0, -4);
+      correctUrl += '.MOV';
+      deffered.resolve(correntUrl);
+    }
+
+    function fail(error) {
+      console.log('FAIL: ' + error.code);
+      deffered.reject('ERROR');
+    }
+
+    function makeid() {
+      var text = '';
+      var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567889';
+      for (var i = 0; i < 5; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+      return text;
+    }
+
+    return {
+      saveVideo: function(data) {
+        createFileEntry(data[0].localURL);
+        return promise;
+      }
+    }
+
+  })
