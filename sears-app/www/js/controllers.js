51495cd19296ffe2e83a0c62d836e45b318f4395
bpshonyak <bpshonyak@live.com>
2015-07-25 21:38:23 -0700

    filter is working!

--- sears-app/www/js/controllers.js
+++ sears-app/www/js/controllers.js
@@ -2,8 +2,37 @@
 
 app.controller('TicketsCtrl', ['$scope', 'techs', 'product_data', '$http', function($scope, techs, product_data, $http) {
 
+        var unique = function(arr) {
+          var i,
+            len = arr.length,
+            out = [],
+            obj = {};
+
+          for (i = 0; i < len; i++) {
+            obj[arr[i]] = 0;
+          }
+          for (i in obj) {
+            out.push(i);
+          }
+          return out;
+        }
+
   $scope.techs = techs.all();
   $scope.products;
+  $scope.brands = [];
+  $scope.types = [
+    'Refrigerators',
+    'Dryers',
+    'Washers',
+    'Cooktops'
+  ];
+
+  $scope.warrantys = [
+    'No Coverage',
+    'Sears Special Protection',
+    'Manufacturer warranty',
+    "Don't Know"
+  ];
 
   console.log('before call');
 
@@ -14,13 +43,18 @@ app.controller('TicketsCtrl', ['$scope', 'techs', 'product_data', '$http', funct
       .success(function(data, status, headers, config) {
         $scope.products = data.SearchResults.Products;
         console.log($scope.products);
-        // console.log('Data: ' + JSON.stringify(data));
+        for (var i = 0; i < $scope.products.length; i++) {
+          $scope.brands.push($scope.products[i].Description.BrandName);
+        }
+        console.log($scope.brands);
+        $scope.brands = unique($scope.brands);
+        console.log($scope.brands);
       }).
-      error(function(data, status, headers, config) {
-        // console.log(data);
-        // console.log(status);
-        // console.log(headers);
-      });
+    error(function(data, status, headers, config) {
+      // console.log(data);
+      // console.log(status);
+      // console.log(headers);
+    });
 
     console.log('end!');
   }
