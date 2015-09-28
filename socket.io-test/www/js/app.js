606949815c0edee029af215bcfebb60289f9630d
bpshonyak <bpshonyak@live.com>
2015-09-27 19:48:58 -0700

    finished chat app

--- socket.io-test/www/js/app.js
+++ socket.io-test/www/js/app.js
@@ -9,11 +9,27 @@ app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('chat', {
+      url: "/chat/:nickname",
+      templateUrl: "templates/chat.html"
+    })
+    .state('login', {
+      url: "/login",
+      templateUrl: "templates/login.html"
+    });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
+});
